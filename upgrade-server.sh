2541bc02bcbc0133f0ffa384e9ee1828c2e960ae
weixuanf <weixuanf@outlook.com>
2025-04-22 17:18:21 +0000

    update upgrade server script

--- upgrade-server.sh
+++ upgrade-server.sh
@@ -11,16 +11,10 @@ fi
 git clone https://github.com/11cafe/code-sandbox-server.git /tmp/code-sandbox-server
 
 # Build the project
-cd /tmp/code-sandbox-server/container_manager
+cp -r /tmp/code-sandbox-server/container_manager/* /runbox/container_manager/
+cd /runbox/container_manager
 npm install
 npm run build
-# move the built dist to /runbox
-mkdir -p /runbox/container_manager
-mv /tmp/code-sandbox-server/container_manager/dist /runbox/container_manager/dist
-# move the nginx.conf to /runbox
-mv /tmp/code-sandbox-server/nginx.conf /etc/nginx/nginx.conf
-# move the start.sh to /runbox
-mv /tmp/code-sandbox-server/start.sh /runbox/start.sh
 
-# restart the server
-sudo /runbox/start.sh
+# restart the server gracefully 0 downtime
+pm2 reload
